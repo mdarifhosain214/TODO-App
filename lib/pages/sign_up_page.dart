bce633b921828f85a8ccb4cf64731a093fdd4bc5
Hosain Arif <mdarifhosain214@gmail.com>
2022-12-30 21:58:06 +0600

    Todo app uploaded

---
+++ lib/pages/sign_up_page.dart
@@ -0,0 +1,213 @@
+import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:todo/pages/homePage.dart';
+import 'package:todo/pages/signInPage.dart';
+import 'package:todo/service/authService.dart';
+
+class SignUpPage extends StatefulWidget {
+  const SignUpPage({Key? key}) : super(key: key);
+
+  @override
+  State<SignUpPage> createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  firebase_auth.FirebaseAuth _auth = firebase_auth.FirebaseAuth.instance;
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+  bool circuler =false;
+  AuthClass authClass = AuthClass();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          color: Colors.black,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Sign Up',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 35,
+                    color: Colors.white),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              buttonItem(
+                  buttonName: 'Continue with google',
+                  imagePath: 'images/google.svg',
+                  buttonSize: 25,
+              onTap: ()async{
+                   await authClass.googleSignIn(context);
+              }),
+              const SizedBox(
+                height: 15,
+              ),
+              buttonItem(
+                  buttonName: 'Continue with Phone',
+                  imagePath: 'images/phone.svg',
+                  buttonSize: 25,
+                onTap: ()async{}
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              const Text(
+                'or',
+                style: TextStyle(fontSize: 16, color: Colors.white),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              textItem(labelText: 'Email...', controller: _emailController,obscureText: false),
+              const SizedBox(
+                height: 15,
+              ),
+              textItem(labelText: 'Password', controller: _passwordController,obscureText: true),
+              const SizedBox(
+                height: 30,
+              ),
+              colorButton(),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children:  [
+                  const Text(
+                    'Already have an account? ',
+                    style: TextStyle(fontSize: 18, color: Colors.white),
+                  ),
+                  InkWell(
+                    onTap: (){
+                      Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (builder)=>SignInPage()), (route) => false);
+                    },
+                    child: const Text(
+                      'Login',
+                      style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget colorButton() {
+    return InkWell(
+      onTap: ()async {
+        setState((){circuler =true;});
+       try{
+         firebase_auth.UserCredential userCredential=await _auth.createUserWithEmailAndPassword(
+             email: _emailController.text,
+             password: _passwordController.text);
+         setState((){circuler =false;});
+         const snackBar =   SnackBar(content: Text("Create account successfully",style: TextStyle(fontSize: 18),));
+         ScaffoldMessenger.of(context).showSnackBar(snackBar);
+         Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context)=>HomePage()), (route) => false);
+
+       }catch(e){
+         final snackBar = SnackBar(content: Text(e.toString(),style: const TextStyle(fontSize: 18)));
+         ScaffoldMessenger.of(context).showSnackBar(snackBar);
+         setState((){circuler =false;});
+
+       }
+      },
+      child: Container(
+        height: 60,
+        width: MediaQuery.of(context).size.width - 100,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            gradient: const LinearGradient(colors: [
+              Color(0xfffd746c),
+              Color(0xffff9068),
+              Color(0xfffd746c),
+            ])),
+        child:  Center(
+            child:circuler? const CircularProgressIndicator(): const Text(
+          'Sign Up',
+          style: TextStyle(
+              color: Colors.white, fontSize: 22, fontWeight: FontWeight.bold),
+        )),
+      ),
+    );
+  }
+
+  Widget buttonItem(
+      {required String imagePath,
+      required String buttonName,
+      required double buttonSize,required Function() onTap}) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        height: 60,
+        width: MediaQuery.of(context).size.width - 60,
+        child: Card(
+          color: Colors.black,
+          elevation: 8,
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15),
+              side: const BorderSide(width: 1, color: Colors.grey)),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SvgPicture.asset(
+                imagePath,
+                height: buttonSize,
+                width: buttonSize,
+              ),
+              const SizedBox(
+                width: 15,
+              ),
+              Text(
+                buttonName,
+                style: const TextStyle(color: Colors.white, fontSize: 17),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget textItem(
+      {required String labelText, required TextEditingController controller,required bool obscureText}) {
+    return Container(
+      height: 55,
+      width: MediaQuery.of(context).size.width - 70,
+      child: TextFormField(
+        obscureText: obscureText,
+        controller: controller,
+        style: const TextStyle(color: Colors.white, fontSize: 17,),
+        decoration: InputDecoration(
+          labelText: labelText,
+          labelStyle: const TextStyle(color: Colors.white, fontSize: 17
+          ),
+          focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(15),
+              borderSide: const BorderSide(
+                  width: 1.5,
+                  color: Colors.amber)),
+          enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(15),
+              borderSide: const BorderSide(
+                  width: 1,
+                  color: Colors.grey)),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/service/authService.dart
@@ -0,0 +1,42 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:todo/pages/homePage.dart';
+
+class AuthClass{
+  GoogleSignIn _googleSignIn = GoogleSignIn(
+    scopes: [
+      'email',
+      'https://www.googleapis.com/auth/contacts.readonly',
+    ],
+  );
+  FirebaseAuth auth =FirebaseAuth.instance;
+  Future<void> googleSignIn(BuildContext context)async{
+    try{
+      GoogleSignInAccount? googleSignInAccount =await _googleSignIn.signIn();
+      if(googleSignInAccount!=null){
+        GoogleSignInAuthentication googleSignInAuthentication = await googleSignInAccount.authentication;
+        AuthCredential credential = GoogleAuthProvider.credential(
+          idToken: googleSignInAuthentication.idToken,
+          accessToken: googleSignInAuthentication.accessToken
+        );
+        try{
+          UserCredential userCredential = await auth.signInWithCredential(credential);
+          Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (builder)=>HomePage()), (route) => false);
+        }catch(e){
+          final snackBar = SnackBar(content: Text(e.toString()));
+          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+        }
+
+      }
+      else{
+        final snackBar = SnackBar(content: Text('Unable to login'));
+        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+      }
+    }catch(e){
+      final snackBar = SnackBar(content: Text(e.toString()));
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    }
+
+  }
+}
